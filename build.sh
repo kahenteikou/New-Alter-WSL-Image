c0a61c4431d9afe08557fafe67dba63319fc8c21
kokkiemouse <kokkiemouse@gmail.com>
2020-06-27 10:21:00 +0900

    fix

--- build.sh
+++ build.sh
@@ -6,12 +6,7 @@ fi
 mkalteriso=$1
 source ./packages.sh
 $mkalteriso -a x86_64 -w work/x86_64 -C pacman-x86_64.conf -D install_dir init
-#cp pacman.conf.nosig work/x86_64/airootfs/etc/pacman.d/pacman.conf
-#cp mirrorlist work/x86_64/airootfs/etc/pacman.d/
-#cp mirrorlist-alter work/x86_64/airootfs/etc/pacman.d/
-#cp sudoers work/x86_64/airootfs/etc/
-cp -r root_folder/ work/x86_64/airootfs/
-#cp adduser-deb-3.113+nmu3-1-any.pkg.tar.xz work/x86_64/airootfs/
+cp -r root_folder/* work/x86_64/airootfs/
 sed -i -e "s/#ja_JP.UTF-8/ja_JP.UTF-8/" work/x86_64/airootfs/etc/locale.gen
 echo "LANG=ja_JP.UTF-8" | tee work/x86_64/airootfs/etc/locale.conf
 rm -rf work/x86_64/airootfs/etc/mtab
